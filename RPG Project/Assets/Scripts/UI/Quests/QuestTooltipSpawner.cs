e6fd6ed2a19a7fbe9283dc8dc33eee0f9931193a
belyaev-ban <belyaev-ban@yandex.ru>
2022-02-13 15:35:45 +0300

    Quest UI tooltips and toggling

---
+++ RPG Project/Assets/Scripts/UI/Quests/QuestTooltipSpawner.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using GameDevTV.Core.UI.Tooltips;
+using UnityEngine;
+
+public class QuestTooltipSpawner : TooltipSpawner
+{
+    public override void UpdateTooltip(GameObject tooltip)
+    {
+        
+    }
+
+    public override bool CanCreateTooltip()
+    {
+        return true;
+    }
+}
